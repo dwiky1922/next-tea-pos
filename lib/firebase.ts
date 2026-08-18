82116e67ea9c63e785412fb08c87421ce637250d
dwiky1922 <a710230033@student.ums.ac.id>
2026-08-18 15:13:04 +0700

    Move Firebase credentials to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,20 +1,20 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // Tambahan untuk Storage gambar
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4SldHmRGmPCiPzL8mQMjyf-D2wonL1us",
-  authDomain: "next-tea-pos.firebaseapp.com",
-  projectId: "next-tea-pos",
-  storageBucket: "next-tea-pos.appspot.com",
-  messagingSenderId: "105743298195",
-  appId: "1:105743298195:web:186fdf7baecf5bf139a766"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app); // Inisialisasi Storage
+const storage = getStorage(app);
 
 export { app, auth, db, storage };
